8c52cac30f9b0cfb775eb6df4ee7a6a6d3197a08
D. Karaba <dkaraba@andrew.cmu.edu>
2011-09-25 14:10:01 -0400

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AfrikApp::Application.config.secret_token = '08701941141cbe79cf5128302cbed023b4fa1a5f770e795f36dac34fccf64aac969a22f79d7a8f03e4b9ddfc41f15be61df4d6e2e0a4f2cac52d59a041b941cd'
